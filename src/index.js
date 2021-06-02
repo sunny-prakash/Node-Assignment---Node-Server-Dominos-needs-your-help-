80c44e844205ddc1f137b95a470fe62b3e300795
Sunny  Prakash <sunny.pprakash@gmail.com>
2021-06-02 09:38:15 +0530

    added

--- src/index.js
+++ src/index.js
@@ -2,9 +2,21 @@ var http = require("http");
 
 const httpServer = http.createServer(handleServer);
 
-
 function handleServer(req, res) {
-  
+    if (req.url === "/welcome") {
+        res.writeHead(200, { "Content-Type": "text/plain" });
+        res.write("Welcome to Dominos!");
+        res.end();
+    }
+    if (req.url === "/contact") {
+        res.writeHead(200, { "Content-Type": "application/json" });
+        res.write({
+            phone: "18602100000",
+            email: "guestcaredominos@jublfood.com",
+        });
+        res.end();
+    }
+    res.writeHead(404);
 }
 
 module.exports = httpServer;
